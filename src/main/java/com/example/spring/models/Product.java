ce6d4d3da6c8e79c1e3677863fed891897fcf14a
Kolomiiets_Yosyp <Kolomiiets204@gmail.com>
2023-03-08 11:16:07 +0200

    add

---
+++ src/main/java/com/example/spring/models/Product.java
@@ -0,0 +1,14 @@
+package com.example.spring.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Product {
+    private long id;
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
